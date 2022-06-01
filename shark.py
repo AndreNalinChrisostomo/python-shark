79187ab916e590b408141e7659c7f4f52ab7d365
André Chrisostomo <71896776+AndreNalinChrisostomo@users.noreply.github.com>
2022-06-01 11:59:02 -0300

    shark.py atualizado
    Adicionado um menu interativo

--- shark.py
+++ shark.py
@@ -1,5 +1,4 @@
-from api import get
-#from api import ip_only
+from api import get_location, ip_only, menu
 import pyshark
 
 #modifique estas variáveis
@@ -11,11 +10,47 @@ iface = 'Wi-Fi'
 capture = pyshark.LiveCapture(interface=iface,display_filter=input("filtro: "))
 
 
-#lista de IPs que ja foram exibidos (não está funcionando, estou tentando descobrir o porquê)
 vistos = []
 
 
 def start():
+	opcao_repeticoes = 0
+	opcao = 0
+	print("""
+		======================
+		1 - pegar localização
+		2 - pegar ip
+		3 - sair
+		======================""")
+
+	escolha = input("digite a opção: ")
+
+	if escolha == "1":
+		opcao = 1
+	elif escolha == "2":
+		opcao = 2
+	elif opcao == "3":
+		opcao = 3
+
+	print("""
+		======================
+		Evitar repetições?
+		1 - Sim
+		2 - Não
+		======================""")
+	escolha_repeticoes = input("digite a opção: ")
+	
+	if escolha_repeticoes == "1":
+		opcao_repeticoes = 1
+	elif escolha_repeticoes == "2":
+		opcao_repeticoes = 2
+	else:
+		exit("opção inválida")
+		
+
+
+
+
 	#loop pelos pacotes que estão sendo capturados
 	for packet in capture.sniff_continuously():
 		#se o seu computador for a fonte do pacote
@@ -27,13 +62,21 @@ def start():
 				if seu_ip_interno in str(packet.ip.src):
 					pass
 				else:
-					#tenta rastrear o ip do pacote
-					get(str(packet.ip.src))
-					#ip_only(str(packet.ip.src))
+					#executa a função escolhida pelo usuário
+					if opcao == 1:
 					
+						get_location(str(packet.ip.src))
+					elif opcao == 2:
+						
+						ip_only(str(packet.ip.src))
+					elif opcao == 3:
+						exit("saindo...")			
 
 				# adiciona o ip de destino na lista 'vistos'
-				vistos.append(str(packet.ip.src))
+				if opcao_repeticoes == 1:
+					vistos.append(str(packet.ip.src))
+				else:
+					pass
 	#fecha a captura
 	capture.close()
 
